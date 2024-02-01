4ff258c768c267fd901e2c9a2f5f5e9510ec7739
Jeff Garzik <jgarzik@pobox.com>
2024-01-31 20:58:48 -0500

    Implement commands: exit, help, version

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,37 @@ struct Args {
     read_only: bool,
 }
 
+fn cmd_help() {
+    let helpstr = r#"Available commands:
+exit		Exit program.
+help		This message.
+version		Display program name and version."#;
+
+    println!("{}", helpstr);
+}
+
+fn cmd_version() {
+    const PKG_NAME: &str = env!("CARGO_PKG_NAME");
+    const VERSION: &str = env!("CARGO_PKG_VERSION");
+
+    println!("{} {}", PKG_NAME, VERSION);
+}
+
+fn handle_line(line: &String) -> bool {
+    let mut tsplit = line.split_whitespace();
+
+    let cmd_name = tsplit.next().expect("No command provided");
+
+    match cmd_name {
+        "exit" => return false,
+        "help" => cmd_help(),
+        "version" => cmd_version(),
+        _ => println!("Invalid or unknown command"),
+    }
+
+    true
+}
+
 fn main() -> Result<()> {
     let args = Args::parse();
 
@@ -27,19 +58,21 @@ fn main() -> Result<()> {
         match readline {
             Ok(line) => {
                 let _ = rl.add_history_entry(line.as_str());
-                println!("Line: {}", line);
-            },
+                if !handle_line(&line) {
+                    break;
+                }
+            }
             Err(ReadlineError::Interrupted) => {
                 println!("CTRL-C");
-                break
-            },
+                break;
+            }
             Err(ReadlineError::Eof) => {
                 println!("CTRL-D");
-                break
-            },
+                break;
+            }
             Err(err) => {
                 println!("Error: {:?}", err);
-                break
+                break;
             }
         }
     }
