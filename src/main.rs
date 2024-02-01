2a4a66c643a53eb6c41bc61a0cdb1917721d6623
Jeff Garzik <jgarzik@pobox.com>
2024-01-31 20:25:29 -0500

    Skeleton CLI parsing, readline loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
-fn main() {
-    println!("Hello, world!");
+extern crate clap;
+extern crate rustyline;
+
+use clap::Parser;
+use rustyline::error::ReadlineError;
+use rustyline::{DefaultEditor, Result};
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Open database in read-only mode?
+    #[arg(short, long, default_value_t = false)]
+    read_only: bool,
+}
+
+fn main() -> Result<()> {
+    let args = Args::parse();
+
+    if args.read_only {
+        println!("Option: read-only");
+    }
+
+    let mut rl = DefaultEditor::new()?;
+
+    loop {
+        let readline = rl.readline("gdbm> ");
+        match readline {
+            Ok(line) => {
+                let _ = rl.add_history_entry(line.as_str());
+                println!("Line: {}", line);
+            },
+            Err(ReadlineError::Interrupted) => {
+                println!("CTRL-C");
+                break
+            },
+            Err(ReadlineError::Eof) => {
+                println!("CTRL-D");
+                break
+            },
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break
+            }
+        }
+    }
+
+    Ok(())
 }
